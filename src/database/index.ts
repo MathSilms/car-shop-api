06c97895790c189bf87b194ec5dd0471f8dadab5
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2022-06-15 23:09:24 -0300

    removendo console.logs

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,7 @@
 import { createConnection, getConnectionOptions } from 'typeorm';
 
 interface IOptions { host: string; } 
-console.log("INDEX DATABASE");
+
 getConnectionOptions().then(options => { 
     const newOptions = options as IOptions; 
     newOptions.host = "database_ignite"; //Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados 
