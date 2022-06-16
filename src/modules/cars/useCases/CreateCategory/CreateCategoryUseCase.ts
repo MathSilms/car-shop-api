389b46e8d454c224a4561256983c551b2a11a4cb
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2022-06-15 23:21:50 -0300

    instalando nova lib

--- src/modules/cars/useCases/CreateCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/CreateCategory/CreateCategoryUseCase.ts
@@ -10,7 +10,7 @@ class CreateCategoryUseCase {
 
    async execute({ name, description }: IRequest): Promise<void> {
         const categoryAlreadyExists = await this.categoriesRepository.findByName(name);
-      
+        
         if (categoryAlreadyExists) throw new Error(' Category already exists!');
 
         // return res.status(400).json({ error: 'Category already exists' });
